bfd01b7e60967ce1b100280c90c33f5153964f76
27097 <tapiasgroup@gmail.com>
2024-05-10 11:08:43 -0300

    local10

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,9 @@ import n from './Nav.module.css'
 
 const Nav = () => {
     return (
-        <>
+        <div className={n.nav}>
             <h1>Nav</h1>
-        </>
+        </div>
     )
 }
 export default Nav
